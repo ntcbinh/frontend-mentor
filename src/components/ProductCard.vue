09acfd02ad3a53f313e50e3cbe59accb08c11f93
ntcbinh <nguyen.thi.cam.binh@i-enter.asia>
2023-11-27 17:12:18 +0700

    init product card

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="product-card">
+    <div class="card">
+      <div class="image" />
+      <div>text</div>
+    </div>
+  </div>
+</template>
